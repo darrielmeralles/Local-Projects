df5e83abb96e5c0d9c19c0616f5c1a5ae3cc5b88
Darriel Meralles <59433113+darrielmeralles@users.noreply.github.com>
2023-02-23 23:02:49 +0800

    Update
    Custom images

---
+++ Dynamic-Page-images/src/js/script.js
@@ -0,0 +1,21 @@
+let element = $('.widget-abc123');
+let data = {
+	device: 'desktop', //desktop, tablet, mobile
+	inEditor: true,
+	siteId: '',
+	elementId: '',
+	config: {
+		sampleList: [{}],
+		sample:''
+	}
+};
+
+
+
+//ADD MULTIPLE LINK SOURCE HERE
+
+
+dmAPI.runOnReady('init', function () {
+
+})
+
