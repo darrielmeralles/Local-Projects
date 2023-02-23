c474ebb5fdafdc9a8800e227ffc003fbf922f78e
Darriel Meralles <59433113+darrielmeralles@users.noreply.github.com>
2023-02-23 22:36:08 +0800

    Done
    Custom Mega Menu

---
+++ Custom-Mega-Menu/src/js/script.js
@@ -0,0 +1,23 @@
+let element = $('.widget-abc123');
+let data = {
+	device: 'desktop', //desktop, tablet, mobile
+	inEditor: true, 
+	siteId: '',
+	elementId: '',
+	config: {
+		sampleList: [{}],
+		sample:''
+	}
+};
+
+
+
+//ADD MULTIPLE LINK SOURCE HERE
+
+
+dmAPI.runOnReady('init', function () {
+	$( ".closeIcon" ).click(function() {
+		alert( "Handler for .click() called." );
+	  });
+})
+
