850d9188c5445638ce8deed29c87e50d6ecd8d4e
Darriel Meralles <59433113+darrielmeralles@users.noreply.github.com>
2023-03-31 13:09:33 +0800

    update
    products done layout 2

--- Product-Listing-Widget/src/js/script.js
+++ Product-Listing-Widget/src/js/script.js
@@ -6,9 +6,32 @@ let data = {
 	elementId: '',
 	config: {
 		sampleList: [{}],
-		sample:''
+		AdditionalImages:'https://irt-cdn.multiscreensite.com/edb6655af63949cc93dd07aa38c7b398/dms3rep/multi/cargal3.jpg,https://irt-cdn.multiscreensite.com/edb6655af63949cc93dd07aa38c7b398/dms3rep/multi/cargal2.jpg,https://irt-cdn.multiscreensite.com/edb6655af63949cc93dd07aa38c7b398/dms3rep/multi/cargal1.jpg,https://irt-cdn.multiscreensite.com/edb6655af63949cc93dd07aa38c7b398/dms3rep/multi/cargal1.jpg,https://irt-cdn.multiscreensite.com/edb6655af63949cc93dd07aa38c7b398/dms3rep/multi/cargal1.jpg,https://irt-cdn.multiscreensite.com/edb6655af63949cc93dd07aa38c7b398/dms3rep/multi/cargal1.jpg,https://irt-cdn.multiscreensite.com/edb6655af63949cc93dd07aa38c7b398/dms3rep/multi/cargal1.jpg,https://irt-cdn.multiscreensite.com/edb6655af63949cc93dd07aa38c7b398/dms3rep/multi/cargal1.jpg,https://irt-cdn.multiscreensite.com/edb6655af63949cc93dd07aa38c7b398/dms3rep/multi/cargal1.jpg',
+		listingSlideToShow: 4,
+		autoPlayListingSlide: false,
+		arrowToggle: true,
+		interval: 3000,
+		animSpeed: 200
 	}
 };
+let eachCarImage = data.config.AdditionalImages;
+let separatedImage = eachCarImage.split(",");
+let listingSlideToShow = data.config.listingSlideToShow;
+let arrowDiv = $(element).find('.arrowsWrapper');
+let arrowToggle = data.config.arrowToggle;
+let autoPlayListingSlide = data.config.autoPlayListingSlide;
+let interval = data.config.interval;
+let animSpeed = data.config.animSpeed;
+
+separatedImage.map(function(i){
+    let output = 
+    `<img src="${i}">`;    
+    $(element).find('.listingGalleryContainer').append(output);
+    
+    let output1 = 
+    `<img src="${i}">`;    
+    $(element).find('.listingGalleryContainerThumbnail').append(output1);
+});
 
 let collection = new Collection()
 
@@ -35,6 +58,34 @@ switch (device) {
 
 
 dmAPI.runOnReady('init', function () {
+	// THUMBNAIL SLIDER
+	dmAPI.loadScript('https://irp-cdn.multiscreensite.com/f49f126e/files/uploaded/slick.min.js', function() { 
+		$(element).find('.listingGalleryContainer').slick({
+			slidesToShow: 1,
+			slidesToScroll: 1,
+			fade: true,
+			infinite:true,
+			autoplay:autoPlayListingSlide,
+			arrows: false,
+			autoplaySpeed:interval,
+			speed:animSpeed,
+			asNavFor: $(element).find('.listingGalleryContainerThumbnail'),
+			prevArrow:'<button type="button" class="cSlickPrev cSarrow"><i class="fa fa-angle-left fa-3x"></i></button>',
+			nextArrow:'<button type="button" class="cSlickNext cSarrow"><i class="fa fa-angle-right fa-3x"></i></button>',
+		});
+		$(element).find('.listingGalleryContainerThumbnail').slick({
+			slidesToShow: parseInt(4),
+			slidesToScroll: 1,
+			infinite:true,
+			asNavFor: $(element).find('.listingGalleryContainer'),
+			dots: false,
+			centerMode: true,
+			centerPadding:0,
+			focusOnSelect: true,
+			arrows:false
+		});
+	});
+	
 	(function(){
 		var $element = $(element);
 		$element.find('#accordion > ol > div').click(function(){
@@ -58,6 +109,6 @@ dmAPI.runOnReady('init', function () {
 		});
 	})();
 
-	
+
 
 })
