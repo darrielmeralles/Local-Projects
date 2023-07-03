ede8e751ba93b376d0ffdbb8d4cf5b58f2ec3ffc
Darriel Meralles <59433113+darrielmeralles@users.noreply.github.com>
2023-07-03 20:58:24 +0800

    Update

---
+++ Vertical-Timeline/src/js/script.js
@@ -0,0 +1,95 @@
+let element = $('.widget-abc123');
+let data = {
+	device: 'desktop', //desktop, tablet, mobile
+	inEditor: true,
+	siteId: '',
+	elementId: '',
+	config: {
+		sampleList: [{}],
+		sample:''
+	}
+};
+
+let collection = new Collection()
+
+let device = data.device;
+let sampleList = data.config.sampleList;
+let sample = data.config.sample;
+
+let noCollectMessage = 'No data was found.' ///data.config.noCollectMessage
+let noCollectSubMessage = 'This will be hidden on preview and live site.' ///data.config.noCollectSubMessage
+let sampleListData;
+
+switch (device) {
+	case 'desktop':
+		$(element).width("960px");
+		break;
+	case 'tablet':
+		$(element).width("875px");
+		break;
+	default:
+		$(element).width("326px");
+}
+
+//ADD MULTIPLE LINK SOURCE HERE
+
+
+dmAPI.runOnReady('init', function () {
+	(function($) {
+		$.fn.timeline = function() {
+		  var selectors = {
+			id: $(this),
+			item: $(this).find(".timeline-item"),
+			activeClass: "timeline-item--active",
+			img: ".timeline__img"
+		  };
+		  selectors.item.eq(0).addClass(selectors.activeClass);
+		  selectors.id.css(
+			"background-image",
+			"url(" +
+			  selectors.item
+				.first()
+				.find(selectors.img)
+				.attr("src") +
+			  ")"
+		  );
+		  var itemLength = selectors.item.length;
+		  $(window).scroll(function() {
+			var max, min;
+			var pos = $(this).scrollTop();
+			selectors.item.each(function(i) {
+			  min = $(this).offset().top;
+			  max = $(this).height() + $(this).offset().top;
+			  var that = $(this);
+			  if (i == itemLength - 2 && pos > min + $(this).height() / 2) {
+				selectors.item.removeClass(selectors.activeClass);
+				selectors.id.css(
+				  "background-image",
+				  "url(" +
+					selectors.item
+					  .last()
+					  .find(selectors.img)
+					  .attr("src") +
+					")"
+				);
+				selectors.item.last().addClass(selectors.activeClass);
+			  } else if (pos <= max - 40 && pos >= min) {
+				selectors.id.css(
+				  "background-image",
+				  "url(" +
+					$(this)
+					  .find(selectors.img)
+					  .attr("src") +
+					")"
+				);
+				selectors.item.removeClass(selectors.activeClass);
+				$(this).addClass(selectors.activeClass);
+			  }
+			});
+		  });
+		};
+	  })(jQuery);
+	  
+	  $("#timeline-1").timeline();
+	  
+})
--- Widget Preview Iframe/src/js/script.js
+++ Widget Preview Iframe/src/js/script.js
@@ -36,5 +36,30 @@ switch (device) {
 
 dmAPI.runOnReady('init', function () {
 
+	function getVimeoId( url ) {
+
+		// Look for a string with 'vimeo', then whatever, then a
+		// forward slash and a group of digits.
+		var match = /vimeo.*\/(\d+)/i.exec( url );
+	  
+		// If the match isn't null (i.e. it matched)
+		if ( match ) {
+		  // The grouped/matched digits from the regex
+		  return match[1];
+		}
+	  }
+
+	console.log(getVimeoId('http://vimeo.com/11918221'), "ID vidmeo")  
+
+	function getValue(VidID){
+		var value= $.ajax({ 
+		   url: `https://vimeo.com/api/oembed.json?url=https%3A//vimeo.com/${VidID}`, 
+		   async: false
+		}).responseText;
+		let val = JSON.parse(value);
+		console.log(val);
+		return val.thumbnail_url;
+	 }
+	 console.log(getValue("112836958"), " vimeo Thumbnail");
 })
 
